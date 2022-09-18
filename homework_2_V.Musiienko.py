f19e26da04fc4cfec9ad8dcf303e7cede1525cf8
Vladek <112932850+godfatherya@users.noreply.github.com>
2022-09-18 19:54:42 +0300

    Create homework_2_V.Musiienko.py

---
+++ homework_2_V.Musiienko.py
@@ -0,0 +1,14 @@
+num = int(input('Enter a number: '))
+
+if num <= -500:
+    print('The number is equal to or less than -500')
+elif (num <= -100) and (num > -500):
+    print('Equal to or less than -100 but greater than -500')
+elif (num <= 0) and (num > -100):
+    print('Less than 0 but greater than -100')
+elif (num >= 0) and (num < 100):
+    print('Equal to or greater than 0 but less than 100')
+elif (num >= 100) and (num < 500):
+    print('Equal to or greater than 100 but less than 500')
+else:
+    print('Equal to or greater than 500')
