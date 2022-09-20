ce252df3c4ac70a17de5421527f86f8abf3e2cd2
Vladek <112932850+godfatherya@users.noreply.github.com>
2022-09-20 15:27:21 +0300

    Update HW4_ex1_V.Musiienko.py

--- HW4_ex1_V.Musiienko.py
+++ HW4_ex1_V.Musiienko.py
@@ -1,31 +1,31 @@
-def rad_to_degr(value, pi):
-    result = (value * 180) / pi
+import math
+
+x1 = math.pi
+def rad_to_degr(value, x1):
+    result = (value * 180) / x1
 
     print(f'\n{value} радиан = {result} градус.\n')
 
-def degr_to_rad(value, pi):
-    result = (value * pi) / 180
+def degr_to_rad(value, x1):
+    result = (value * x1) / 180
 
     print(f'\n{value} градус = {result} радиан.\n')
 
-
 def main():
-    answer = float(input('''\nВыберите одну из следующих вариантов (Введите номер варианта*):
+    answer = float(input('''\nВыберите один из следующих вариантов (Введите номер варианта*):
     1) радианы в градусы(угол).
     2) градусы в радианы.\n >>> '''))
 
-    pi = 22/7
-
     if answer == 1:
         value = float(input('\nВведите знчение в радианах(Например: 6 ): >>> '))
-        rad_to_degr(value, pi)
+        rad_to_degr(value, x1)
 
     elif answer == 2:
         value = float(input('\nВведите знчение в градусах(Например: 128.8 или 95 ): >>> '))
-        degr_to_rad(value, pi)
+        degr_to_rad(value, x1)
 
     else:
-        print('\n(!) Убедитесь в правильности введенных данных и перезапустите программу*\n')
+        print('\n(!) Убедитесь в правильности введенных данных\n')
 
 
 if __name__ == '__main__':
